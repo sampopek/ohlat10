58446eb16159194c478d36a29435dd24d83dabf0
sampo-priva <sampo-priva@pop-os.localdomain>
2022-12-07 13:56:25 +0200

    initial commit

---
+++ ohlat10/src/main/java/com/ohlat10/Pelaaja.java
@@ -0,0 +1,40 @@
+package com.ohlat10;
+
+/**
+ *
+ * @author Ira Dook
+ */
+public class Pelaaja {
+
+    int voitot; // Voittojen lukumäärä
+    int voitotYhteensä;
+
+    /**
+     * Valitse randomilla kivi, paperi tai sakset
+     */
+    public String pelaajanValinta() {
+        String valinta = "";
+        int c = (int) (Math.random() * 3);
+        switch (c) {
+            case 0:
+                valinta = ("kivi");
+                break;
+            case 1:
+                valinta = ("paperi");
+                break;
+            case 2:
+                valinta = ("sakset");
+                break;
+        }
+        return valinta;
+    }
+
+    public int setVoitot() {
+        int voitotYhteensä = voitot++;
+        return voitotYhteensä;
+    }
+
+    public int getVoitot() {
+        return (voitot);
+    }
+}
